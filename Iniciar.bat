86c9597622044c4fcb0c014eeb3e4791233046ee
Jorgeotero1998 <jorgeotero1998@gmail.com>
2026-04-15 17:31:56 -0300

    cambios

--- Iniciar.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-title SENTINEL SOC ELITE BY JORGE OTERO
-echo [*] Iniciando sistema desde %cd%...
-python src/monitor.py
-pause
--- Lanzar_Sentinel.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title SENTINEL SOC BY JORGE OTERO
-color 0A
-echo [*] Sentinel SOC: Verificando entorno...
-pip install watchdog python-dotenv --quiet
-cls
-python src/monitor.py
-pause
